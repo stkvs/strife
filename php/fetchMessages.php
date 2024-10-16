52449ae439967d252ffac99cb98231bcb9aa6f10
Mantas Statkevicius <stmantaz@gmail.com>
2024-10-16 14:11:38 +0100

    Added highlighted mentioning

--- php/fetchMessages.php
+++ php/fetchMessages.php
@@ -2,17 +2,35 @@
 session_start();
 include './db.php';
 
+$current_user = $_SESSION['username'];
+?>
+
+<?php
 $sql_group_messages = "SELECT gm.message, u.username, gm.sent_at
                        FROM group_messages gm
                        JOIN users u ON gm.user_id = u.id
+                       WHERE u.id IS NOT NULL
                        ORDER BY gm.sent_at DESC";
 $result_group_messages = $conn->query($sql_group_messages);
 
 if ($result_group_messages->num_rows > 0) {
     while ($message = $result_group_messages->fetch_assoc()) {
         $message_text = htmlspecialchars($message['message']);
-        $message_text = preg_replace('/@(\w+)/', '<span class="mention">@${1}</span>', $message_text);
-        echo "<li><b>" . htmlspecialchars($message['username']) . ":</b> " . $message_text . " <i>(" . htmlspecialchars($message['sent_at']) . ")</i></li>";
+        
+        // Find all mentions in the message
+        preg_match_all('/@(\w+)/', $message['message'], $mentions);
+        
+        foreach ($mentions[1] as $mention) {
+            $message_text = preg_replace('/@' . preg_quote($mention, '/') . '/', '<span class="mention">@' . $mention . '</span>', $message_text);
+            if ($mention === $current_user) {
+                // Replace the mention with highlighted span if user is the current user
+                $highlight_class = 'highlight';
+            } else {
+                $highlight_class = '';
+            }
+        }
+        
+        echo "<li class=\"$highlight_class\"><b>" . htmlspecialchars($message['username']) . ":</b> " . $message_text . " <i>(" . htmlspecialchars($message['sent_at']) . ")</i></li>";
     }
 } else {
     echo "<li>No messages in the public group yet.</li>";
