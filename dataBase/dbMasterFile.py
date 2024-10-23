ae05319888e156b8bf436cc0cdd72cf104ff4e3b
SaolGhra <DeathCRystalYT@Gmail.com>
2024-10-23 11:57:39 +0100

    V1.5.3

---
+++ dataBase/dbMasterFile.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+
+def run_script(script_path):
+    if os.path.exists(script_path):
+        try:
+            subprocess.run(['python', script_path], check=True)
+            print(f"Successfully ran {script_path}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running {script_path}: {e}")
+    else:
+        print(f"Script {script_path} does not exist.")
+
+def main():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    scripts = [
+        os.path.join(base_dir, 'database-dependencies', 'updatedb.py'),
+        os.path.join(base_dir, 'database-dependencies', 'makeadmintable.py')
+    ]
+
+    for script in scripts:
+        run_script(script)
+
+if __name__ == "__main__":
+    main()
